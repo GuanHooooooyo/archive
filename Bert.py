ba68c76665dba78e2089968fc242c1ffdcf6d71f
riven <always167486@gmail.com>
2020-07-13 22:05:24 +0800

    train model sample

--- Bert.py
+++ Bert.py
@@ -56,4 +56,26 @@ for i, (t, p) in enumerate(zip(predicted_tokens, probs), 1):
     tokens[masked_index] = t
     print("Top {} ({:2}%)：{}".format(i, int(p.item() * 100), tokens[:15]), '...')
 
+df_train = pd.read_csv('train.csv')
+empty_title = ((df_train['title2_zh'].isnull()) \
+               | (df_train['title1_zh'].isnull()) \
+               | (df_train['title2_zh'] == '') \
+               | (df_train['title2_zh'] == '0'))
+df_train = df_train[~empty_title]
+# 剔除過長的樣本以避免 BERT 無法將整個輸入序列放入記憶體不多的 GPU
+MAX_LENGTH = 30
+df_train = df_train[~(df_train.title1_zh.apply(lambda x : len(x)) > MAX_LENGTH)]
+df_train = df_train[~(df_train.title2_zh.apply(lambda x : len(x)) > MAX_LENGTH)]
+# 只用 1% 訓練數據看看 BERT 對少量標註數據有多少幫助
+SAMPLE_FRAC = 0.01
+df_train = df_train.sample(frac=SAMPLE_FRAC, random_state=9527)
+# 去除不必要的欄位並重新命名兩標題的欄位名
+df_train = df_train.reset_index()
+df_train = df_train.loc[:, ['title1_zh', 'title2_zh', 'label']]
+df_train.columns = ['text_a', 'text_b', 'label']
 
+# idempotence, 將處理結果另存成 tsv 供 PyTorch 使用
+df_train.to_csv("train.tsv", sep="\t", index=False)
+
+print("訓練樣本數：", len(df_train))
+print(df_train.head(5))
